41ba81b588c1b2fb03339ed6e069b3bae3152d51
Sined97 <sin109704@gmail.com>
2025-11-10 09:02:56 -0600

    Fix conexión a PostgreSQL Render

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,15 +1,18 @@
 <?php
-// Conexión a PostgreSQL en Render
 $host = getenv("DB_HOST");
 $port = getenv("DB_PORT");
 $dbname = getenv("DB_NAME");
 $user = getenv("DB_USER");
 $password = getenv("DB_PASS");
 
-$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+$conn_string = "host=$host port=$port dbname=$dbname user=$user password=$password";
+
+$conn = pg_connect($conn_string);
 
 if (!$conn) {
-    die("❌ Error de conexión: " . pg_last_error());
+    echo "❌ Error de conexión: " . pg_last_error();
+    exit;
+} else {
+    echo "✅ Conexión exitosa a la base de datos PostgreSQL ($dbname)";
 }
-// echo "✅ Conexión exitosa a PostgreSQL en Render.";
 ?>
